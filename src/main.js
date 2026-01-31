55d5ecddf5af9dba78bb3bb1fe97d7fbdf512906
Mircha Emanuel `ryuujin` D'Angelo <mirchaemanuel@gmail.com>
2026-01-31 17:51:19 +0100

    Fix CSP to allow JW.org video playback
    Add jw-cdn.org domains to Content-Security-Policy:
    - connect-src: Allow API calls to b.jw-cdn.org
    - media-src: Allow video playback from jw-cdn.org CDN

--- src/main.js
+++ src/main.js
@@ -47,7 +47,7 @@ const SECURITY_HEADERS = {
   'X-Frame-Options': 'DENY',
   'X-XSS-Protection': '1; mode=block',
   'Referrer-Policy': 'no-referrer',
-  'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'"
+  'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://b.jw-cdn.org https://*.jw-cdn.org; media-src 'self' https://*.jw-cdn.org https://cfp2.jw-cdn.org"
 };
 
 /**
